573ecbebb49c589bea1907d177af24cdc4057e70
Icona <96086902+CryptoKnight360@users.noreply.github.com>
2026-01-14 09:17:02 -0500

    Update run.sh

--- openvino_whisper/run.sh
+++ openvino_whisper/run.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bashio
 
+# Use bashio to get config
 MODEL=$(bashio::config 'model' 'openai/whisper-large-v3-turbo')
 DEVICE="CPU"
 
 bashio::log.info "Starting Wyoming OpenVINO Server for LattePanda..."
 
-# Run the python script from the /app directory
+# Run the python script
 python3 /app/wyoming_server.py \
     --model "$MODEL" \
     --device "$DEVICE"
